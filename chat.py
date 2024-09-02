afe7a03846970c6310a82d3425d6989e52466fe0
Rick Umali <rickumali@gmail.com>
2024-09-02 23:37:50 +0000

    Adds more sophisticated prompting
    Lecture 17

--- chat.py
+++ chat.py
@@ -2,11 +2,17 @@ from ctransformers import AutoModelForCausalLM
 
 llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
 
-prompt = """What is the name of the capital city of India, she asked.
-          Please only respond with the city name and then stop talking. 
-          He answered: """
 
-for word in llm(prompt, stream=True):
+def get_prompt(instruction: str) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    print(prompt)
+    return prompt
+
+
+question = "Which city is the capital of India?"
+
+for word in llm(get_prompt(question), stream=True):
     print(word, end="", flush=True)
 
 print()
