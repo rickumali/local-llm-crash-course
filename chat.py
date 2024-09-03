219f19888d7b8bed17d6c966192eb7c8cb720fa9
Rick Umali <rickumali@gmail.com>
2024-09-03 00:11:22 +0000

    Replacing system with Zoltan's
    This system value makes it work better!

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", mode
 
 
 def get_prompt(instruction: str) -> str:
-    system = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
     print(prompt)
     return prompt
