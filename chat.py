8a80586c9b5b7e3ee7ed844c4dd5757ec556ad4b
Rick Umali <rickumali@gmail.com>
2024-09-05 00:16:21 +0000

    Adds conversational history to get_prompt
    Lecture 18

---
+++ chat.py
@@ -0,0 +1,36 @@
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+
+
+def get_prompt(instruction: str, history: list[str] = None) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if history is not None:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n"
+    print(prompt)
+    return prompt
+
+
+history = []
+
+question = "Which city is the capital of India?"
+
+answer = ""
+for word in llm(get_prompt(question), stream=True):
+    print(word, end="", flush=True)
+    answer += word
+
+print()
+
+history.append(answer)
+
+question = "And which is of the United States?"
+
+answer = ""
+for word in llm(get_prompt(question, history), stream=True):
+    print(word, end="", flush=True)
+    answer += word
+
+print()
