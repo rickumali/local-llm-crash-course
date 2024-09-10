032775fdba1cf43dd7a6cf7909ed1509ad4adda5
Rick Umali <rickumali@gmail.com>
2024-09-10 00:25:33 +0000

    Updates chat.py to fully use Chainlit
    Lecture 22

--- chat.py
+++ chat.py
@@ -1,8 +1,6 @@
 import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
-
 
 def get_prompt(instruction: str, history: list[str] = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way."
@@ -16,8 +14,16 @@ def get_prompt(instruction: str, history: list[str] = None) -> str:
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
+    prompt = get_prompt(message.content)
+    response = llm(prompt)
     await cl.Message(response).send()
+
+
+@cl.on_chat_start
+def on_chat_start():
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+
 
 """
 history = []
