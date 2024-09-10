17aca50604ef60175447a5fa794b89235f80d273
Rick Umali <rickumali@gmail.com>
2024-09-10 23:35:56 +0000

    Cleans out commented-out code
    First part of Lecture 24

--- chat.py
+++ chat.py
@@ -8,7 +8,6 @@ def get_prompt(instruction: str, history: list[str] = None) -> str:
     if history is not None:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
-    print(prompt)
     return prompt
 
 
@@ -27,28 +26,3 @@ async def on_message(message: cl.Message):
 def on_chat_start():
     global llm
     llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
-
-
-"""
-history = []
-
-question = "Which city is the capital of India?"
-
-answer = ""
-for word in llm(get_prompt(question), stream=True):
-    print(word, end="", flush=True)
-    answer += word
-
-print()
-
-history.append(answer)
-
-question = "And which is of the United States?"
-
-answer = ""
-for word in llm(get_prompt(question, history), stream=True):
-    print(word, end="", flush=True)
-    answer += word
-
-print()
-"""
