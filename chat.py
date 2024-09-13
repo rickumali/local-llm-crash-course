53b68292c3e68fe26360ad5b7be7e4066c9e91a4
Rick Umali <rickumali@gmail.com>
2024-09-13 00:04:31 +0000

    Implements model switching
    Assignment 3
    
    There's a bunch of repeated code. But it seems to be doing it

--- chat.py
+++ chat.py
@@ -2,10 +2,22 @@ import chainlit as cl
 from ctransformers import AutoModelForCausalLM
 
 
+def check_model_changed(string):
+    words = string.split()
+    if len(words) >= 2 and words[0] == "Model" and words[1] == "changed":
+        return True
+    else:
+        return False
+
+
 def get_prompt(instruction: str, history: list[str] = None) -> str:
     if instruction.lower() == "forget everything":
         history.clear()
         prompt = "forget everything"
+    elif instruction.lower() == "use orca":
+        prompt = "Model changed to orca"
+    elif instruction.lower() == "use llama":
+        prompt = "Model changed to Llama"
     else:
         system = "You are an AI assistant that gives helpful answers. You answer the questions in a short and concise way."
         prompt = f"### System:\n{system}\n\n### User:\n"
@@ -18,6 +30,7 @@ def get_prompt(instruction: str, history: list[str] = None) -> str:
 @cl.on_message
 async def on_message(message: cl.Message):
     message_history = cl.user_session.get("message_history")
+    model = cl.user_session.get("model")
     msg = cl.Message(content='')
     await msg.send()
 
@@ -26,10 +39,20 @@ async def on_message(message: cl.Message):
     if prompt == "forget everything":
         response = "Uh oh, I've just forgotten our conversation history."
         await msg.stream_token(response)
+    elif check_model_changed(prompt):
+        await msg.stream_token(prompt)
     else:
-        for word in llm(prompt, stream=True):
-            await msg.stream_token(word)
-            response += word
+        if model == "orca":
+            for word in llm_orca(prompt, stream=True):
+                await msg.stream_token(word)
+                response += word
+        elif model == "llama":
+            for word in llm_llama(prompt, stream=True):
+                await msg.stream_token(word)
+                response += word
+        else:
+            response = f"Uh oh. I don't know {model}"
+            await msg.stream_token(response)
     await msg.update()
     message_history.append(response)
 
@@ -37,5 +60,8 @@ async def on_message(message: cl.Message):
 @cl.on_chat_start
 def on_chat_start():
     cl.user_session.set("message_history", [])
-    global llm
-    llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+    global llm_orca
+    llm_orca = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+    global llm_llama
+    llm_llama = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
+    cl.user_session.set("model", "orca")
